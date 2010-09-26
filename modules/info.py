7c18a1f740b0e2d756d8eb90fe291a011dc570af
Michael Yanovich <yanovich.1@osu.edu>
2010-09-26 00:59:05 -0400

    updated phenny to m5

--- modules/info.py
+++ modules/info.py
@@ -1,58 +1,58 @@
 #!/usr/bin/env python
 """
-info.py - Phenny Information Module
+info.py - m5 Information Module
 Copyright 2008, Sean B. Palmer, inamidst.com
 Licensed under the Eiffel Forum License 2.
 
 http://inamidst.com/phenny/
 """
 
-def doc(phenny, input): 
+def doc(m5, input): 
    """Shows a command's documentation, and possibly an example."""
    name = input.group(1)
    name = name.lower()
 
-   if phenny.doc.has_key(name): 
-      phenny.reply(phenny.doc[name][0])
-      if phenny.doc[name][1]: 
-         phenny.say('e.g. ' + phenny.doc[name][1])
+   if m5.doc.has_key(name): 
+      m5.reply(m5.doc[name][0])
+      if m5.doc[name][1]: 
+         m5.say('e.g. ' + m5.doc[name][1])
 doc.rule = ('$nick', '(?i)(?:help|doc) +([A-Za-z]+)(?:\?+)?$')
 doc.example = '$nickname: doc tell?'
 doc.priority = 'low'
 
-def commands(phenny, input): 
+def commands(m5, input): 
    # This function only works in private message
    #if input.sender.startswith('#'): return
-   names = ', '.join(sorted(phenny.doc.iterkeys()))
-   phenny.say("I am sending you a private message of all my commands!")
-   phenny.msg(input.nick, 'Commands I recognise: ' + names + '.')
-   phenny.msg(input.nick, ("For help, do '%s: help example?' where example is the " + 
-               "name of the command you want help for.") % phenny.nick)
+   names = ', '.join(sorted(m5.doc.iterkeys()))
+   m5.say("I am sending you a private message of all my commands!")
+   m5.msg(input.nick, 'Commands I recognise: ' + names + '.')
+   m5.msg(input.nick, ("For help, do '%s: help example?' where example is the " + 
+               "name of the command you want help for.") % m5.nick)
 commands.commands = ['commands']
 commands.priority = 'low'
 
-#def commandsf(phenny, input):
-#   commands(phenny, input)
+#def commandsf(m5, input):
+#   commands(m5, input)
 #commandsf.commands = ['commands']
 
-def help(phenny, input): 
+def help(m5, input): 
    response = (
       'Hi, I\'m a bot. Say ".commands" to me in private for a list ' + 
-      'of my commands, or see http://inamidst.com/phenny/ for more ' + 
+      'of my commands, or see http://inamidst.com/m5/ for more ' + 
       'general details. My owner is %s.'
-   ) % phenny.config.owner
-   phenny.reply(response)
+   ) % m5.config.owner
+   m5.reply(response)
 help.rule = ('$nick', r'(?i)help(?:[?!]+)?$')
 help.priority = 'low'
 
-def stats(phenny, input): 
+def stats(m5, input): 
    """Show information on command usage patterns."""
    commands = {}
    users = {}
    channels = {}
 
    ignore = set(['f_note', 'startup', 'message', 'noteuri'])
-   for (name, user), count in phenny.stats.iteritems(): 
+   for (name, user), count in m5.stats.iteritems(): 
       if name in ignore: continue
       if not user: continue
 
@@ -74,19 +74,19 @@ def stats(phenny, input):
    creply = 'most used commands: '
    for count, command in comrank[:10]: 
       creply += '%s (%s), ' % (command, count)
-   phenny.say(creply.rstrip(', '))
+   m5.say(creply.rstrip(', '))
 
    # most heavy users
    reply = 'power users: '
    for count, user in userank[:10]: 
       reply += '%s (%s), ' % (user, count)
-   phenny.say(reply.rstrip(', '))
+   m5.say(reply.rstrip(', '))
 
    # most heavy channels
    chreply = 'power channels: '
    for count, channel in charank[:3]: 
       chreply += '%s (%s), ' % (channel, count)
-   phenny.say(chreply.rstrip(', '))
+   m5.say(chreply.rstrip(', '))
 stats.commands = ['stats']
 stats.priority = 'low'
 
